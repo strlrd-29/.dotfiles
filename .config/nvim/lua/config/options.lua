a427cf22aa206b52fad92b5d32065738f8c3f530
Ouassim Abdelmalek Ghribi <oag@big-mama.io>
2024-09-21 19:34:19 +0100

    disabled swap files

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -35,7 +35,8 @@ vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = ""
-
+vim.opt.swapfile = false
+--
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" })
 
