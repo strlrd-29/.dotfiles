f7d86bb192d15c30399c760374f24694cd338835
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2024-11-22 23:27:36 +0100

    chore: removed old config

--- .config/nvim/init.lua
+++
@@ -1,5 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
--- globals file with helper fns for plugin development
-require("globals")
--- .config/nvim/lua/plugins/disabled.lua
+++
@@ -1,1 +0,0 @@
-return { { "nvim-neo-tree/neo-tree.nvim", enabled = false } }
